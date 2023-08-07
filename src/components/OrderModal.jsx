3501b33732f8aa081afe4405e03243ea4f34233d
Meghan-Savage <MSavage1@protonmail.com>
2023-08-07 02:20:48 -0600

    Order validation

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -1,27 +1,87 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./styles/OrderModal.module.css";
 
 function OrderModal({ order, setOrderModal }) {
+  const navigate = useNavigate();
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [addressError, setAddressError] = useState("");
+
+  const isValidPhoneNumber = (number) => {
+    const phoneRegex = /^[\d-()]+$/;
+    return phoneRegex.test(number);
+  };
+
+  const formatPhoneNumber = (number) => {
+    const digitsOnly = number.replace(/\D/g, "");
+    return `(${digitsOnly.slice(0, 3)}) ${digitsOnly.slice(
+      3,
+      6
+    )}-${digitsOnly.slice(6, 10)}`;
+  };
+
+  const isFormValid = () => {
+    let valid = true;
+
+    if (name.trim() === "") {
+      setNameError("Please enter your name.");
+      valid = false;
+    } else {
+      setNameError("");
+    }
+
+    if (phone.trim() === "") {
+      setPhoneError("Please enter your phone number.");
+      valid = false;
+    } else if (!isValidPhoneNumber(phone)) {
+      setPhoneError("Please enter a valid phone number.");
+      valid = false;
+    } else {
+      setPhoneError("");
+    }
+
+    if (address.trim() === "") {
+      setAddressError("Please enter your address.");
+      valid = false;
+    } else {
+      setAddressError("");
+    }
+
+    return valid;
+  };
 
   const placeOrder = async () => {
-    const response = await fetch("/api/orders", {
+    if (!isFormValid()) {
+      return;
+    }
+
+    const formattedPhone = formatPhoneNumber(phone);
+
+    const response = await fetch("http://localhost:3001/api/orders", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
         name,
-        phone,
+        phone: formattedPhone,
         address,
         items: order
       })
     });
-    const data = await response.json();
-    console.log(data);
+
+    if (response.ok) {
+      const data = await response.json();
+      navigate(`/order-confirmation/${data.id}`);
+    } else {
+      throw new Error("500 - Internal Server Error");
+    }
   };
+
   return (
     <>
       <div
@@ -43,39 +103,33 @@ function OrderModal({ order, setOrderModal }) {
             <label htmlFor="name">
               Name
               <input
-                onChange={(e) => {
-                  e.preventDefault();
-                  setName(e.target.value);
-                }}
+                onChange={(e) => setName(e.target.value)}
                 type="text"
                 id="name"
               />
+              <span className={styles.error}>{nameError}</span>
             </label>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="phone">
               Phone
               <input
-                onChange={(e) => {
-                  e.preventDefault();
-                  setPhone(e.target.value);
-                }}
-                type="phone"
+                onChange={(e) => setPhone(e.target.value)}
+                type="text"
                 id="phone"
               />
+              <span className={styles.error}>{phoneError}</span>
             </label>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="address">
               Address
               <input
-                onChange={(e) => {
-                  e.preventDefault();
-                  setAddress(e.target.value);
-                }}
-                type="phone"
+                onChange={(e) => setAddress(e.target.value)}
+                type="text"
                 id="address"
               />
+              <span className={styles.error}>{addressError}</span>
             </label>
           </div>
         </form>
@@ -88,9 +142,7 @@ function OrderModal({ order, setOrderModal }) {
             Close
           </button>
           <button
-            onClick={() => {
-              placeOrder();
-            }}
+            onClick={() => placeOrder()}
             className={styles.orderModalPlaceOrder}
           >
             Place Order
