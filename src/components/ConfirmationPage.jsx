06ea1fcd3e7138c198af1d7c1b16be6a2cf9b287
Meghan-Savage <MSavage1@protonmail.com>
2023-08-07 01:46:56 -0600

    Confirmation Page

---
+++ src/components/ConfirmationPage.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./styles/ConfirmationPage.module.css";
+import OrderConfirmation from "./OrderConfirmation";
+
+function ConfirmationPage() {
+  const { id } = useParams();
+  const [order, setOrder] = useState(null);
+
+  const fetchOrder = async (orderId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/orders/${orderId}`
+      );
+      const data = await response.json();
+      setOrder(data);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrder(id);
+  }, [id]);
+
+  return (
+    <div className={styles["confirmation-page"]}>
+      {order ? <OrderConfirmation order={order} /> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default ConfirmationPage;
